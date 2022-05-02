e3b555a2ec68c00b1f7586083d97598fd64e7939
imagi2rey <amandeepgujjar0001@gmail.com>
2022-05-02 11:40:58 +0000

    all files are ready to be deploy to main branch

---
+++ build/precache-manifest.8abf218c469941d891766caf523a93cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7966d44f6f0055196d21764290e7fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5254985fb441360e327b",
+    "url": "/static/css/main.ebfde1ba.chunk.css"
+  },
+  {
+    "revision": "d6eea6313479556aafce",
+    "url": "/static/js/2.28efb9d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28efb9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5254985fb441360e327b",
+    "url": "/static/js/main.f020bb9e.chunk.js"
+  },
+  {
+    "revision": "fabc6fd72e82fa054f36",
+    "url": "/static/js/runtime-main.c9565b16.js"
+  }
+]);
